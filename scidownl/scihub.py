f0bf1a7a159bbb70bfa6501a689d8f0b2302dc8b
Tishacy <Tishacy@gmail.com>
2019-04-11 14:13:38 +0800

    Add the part of updating available scihub links.

--- scidownl/scihub.py
+++ scidownl/scihub.py
@@ -9,10 +9,10 @@ from termcolor import colored
 from update_link import update_link
 
 
-STD_INFO = colored('[INFO]: ', 'green')
-STD_ERROR = colored('[ERROR]: ', 'green')
-STD_WARNING = colored('[WARNING]', 'green')
-# print(STD_INFO, STD_ERROR, STD_WARNING)
+STD_INFO = colored('[INFO] ', 'green')
+STD_ERROR = colored('[ERROR] ', 'red')
+STD_WARNING = colored('[WARNING] ', 'yellow')
+STD_INPUT = colored('[INPUT] ', 'blue')
 
 class SciHub(object):
     def __init__(self, doi, out):
@@ -21,10 +21,10 @@ class SciHub(object):
         self.sess = requests.Session()
 
     def read_available_links(self):
-        print('[INFO]: Reading available links of Scihub...')
+        print(STD_INFO + 'Reading available links of Scihub...')
         with open('./link.txt', 'r') as f:
             self.scihub_url_list = [l[:-1] for l in f.readlines()]
-        print("[INFO]: Successfully read available links of Scihub.")
+        print(STD_INFO + "Successfully read available links of Scihub.")
 
     def update_link(self, mod='c'):
         update_link(mod)
@@ -34,8 +34,8 @@ class SciHub(object):
         soup = BeautifulSoup(html, 'lxml')
         self.pdf_url = soup.find('iframe', {'id': 'pdf'}).attrs['src'].split('#')[0]
         self.title = ' '.join(self._trim(soup.title.text.split('|')[1]).split('/'))
-        print("[INFO]: PDF url -> \n\t%s" %(self.pdf_url))
-        print("[INFO]: Article title -> \n\t%s" %(self.title))
+        print(STD_INFO + "PDF url -> \n\t%s" %(self.pdf_url))
+        print(STD_INFO + "Article title -> \n\t%s" %(self.title))
 
     def is_captcha_page(self, html):
         try:
@@ -55,7 +55,7 @@ class SciHub(object):
 
         captcha_data = {}
         captcha_data['id'] = soup.find('input', {'type':'hidden', 'name':'id'}).attrs['value']
-        captcha_data['answer'] = input('[INPUT]: Type the captcha: ')
+        captcha_data['answer'] = input(STD_INPUT + 'Type the captcha: ')
         res = self.sess.post(self.pdf_url, data=captcha_data)
         return res
 
@@ -64,35 +64,35 @@ class SciHub(object):
             res = self.sess.get(self.pdf_url)
             html = res.content.decode('latin1')
             if self.is_captcha_page(html):
-                print("[INFO]: Captcha is required.")
+                print(STD_INFO + "Captcha is required.")
                 self.process_captcha_code(html)
             else:
-                print("[INFO]: Verification success.")
+                print(STD_INFO + "Verification success.")
                 break
-        print("[INFO]: Downloading...")
+        print(STD_INFO + "Downloading...")
         wget.download(self.pdf_url, out='%s/%s.pdf' %(self.out, self.title))
 
     def download(self):
         scihub_url_index = 0
         while True:
             if scihub_url_index >= len(self.scihub_url_list):
-                print('[WARNING]: All Scihub links are invalid.')
-                update_req = input('[INPUT]: Would you like to update Scihub links? (y/n): ')
+                print(STD_WARNING + 'All Scihub links are invalid.')
+                update_req = input(STD_INPUT + 'Would you like to update Scihub links? (y/n): ')
                 if update_req == 'y':
                     self.update_link(mod = 'c')
                     self.download()
                 elif update_req == 'n':
-                    print("[INFO]: Please manually update Scihub links by $????")
+                    print(STD_INFO + "Please manually update Scihub links by $????")
                 return
 
             url_pre = self.scihub_url_list[scihub_url_index]
-            print('[INFO]: Choose the available link %d: %s' %(scihub_url_index, url_pre))
+            print(STD_INFO + 'Choose the available link %d: %s' %(scihub_url_index, url_pre))
             paper_url = '%s/%s' %(url_pre, str(self.doi))
 
             res = self.sess.get(paper_url)
             content = res.content.decode('utf-8')
             if content in ['\n', '']:
-                print("[ERROR]: Current Scihub link is invalid, changing another link...")
+                print(STD_ERROR + "Current Scihub link is invalid, changing another link...")
                 scihub_url_index += 1
             else:
                 break
@@ -101,7 +101,7 @@ class SciHub(object):
             self.find_pdf_in_html(content)
             self.download_pdf()
         except:
-            print("[ERROR]: Failed to access the article.")
+            print(STD_ERROR + "Failed to access the article.")
 
     def _trim(self, s):
         """Drop spaces located in the head or the end of the given string.
---
+++ scidownl/update_link.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""Update available Scihub links
+
+method 1: Brute force search
+method 2: Crawling the website https://sci-hub.top, which updates available Scihub links every 30 mins.
+"""
+import string, requests, re
+from bs4 import BeautifulSoup
+from mspider.spider import MSpider
+from termcolor import colored
+
+LETTERS = list(string.ascii_lowercase)
+STD_INFO = colored('[INFO] ', 'green')
+STD_ERROR = colored('[ERROR] ', 'red')
+STD_WARNING = colored('[WARNING] ', 'yellow')
+STD_INPUT = colored('[INPUT] ', 'blue')
+
+def get_url_list():
+    url_list = []
+    url_pre = 'http://sci-hub.'
+    url_pre2 = 'https://sci-hub.'
+    for first_letter in LETTERS:
+        for last_letter in LETTERS:
+            url = url_pre + first_letter + last_letter
+            url2 = url_pre2 + first_letter + last_letter
+            url_list.extend([url, url2])
+    return url_list
+
+def basic_func(index, url):
+    try:
+        html = requests.get(url, timeout=3).content
+        soup = BeautifulSoup(html, 'lxml')
+        title = soup.title.contents[0]
+        if title[:7] == "Sci-Hub":
+            print("\n%s%s" %(STD_INFO, url))
+            LINK_FILE.write(url + '\n')
+        else:
+            print("\r%spassing...".ljust(60) %(STD_INFO), end='')
+    except:
+        print("\r%spassing...".ljust(60) %(STD_INFO), end='')
+
+def update_link(mod='c'):
+    LINK_FILE = open('./link.txt', 'w', encoding='utf-8')
+    print(STD_INFO + "Updating links ...")
+    if mod == 'c':
+        html = requests.get("https://sci-hub.top/").content.decode()
+        pattern = r">(htt[^:]+://sci-hub.[^<]+)<"
+        available_links = re.findall(pattern, html)
+        for link in available_links:
+            print(STD_INFO + "%s" %(link))
+            LINK_FILE.write(link + '\n')
+    elif mod == 'b':
+        spider = MSpider(basic_func, get_url_list(), batch_size=18)
+        spider.crawl()
+    LINK_FILE.close()
+
+
+if __name__=="__main__":
+    update_link('b')
