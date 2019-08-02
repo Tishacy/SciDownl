2817de9adfc87ea0011b64da6cfc6fd4a312b6fd
Tishacy <Tishacy@gmail.com>
2019-08-03 00:31:37 +0800

    Reconstruct code and add -c arguement.

--- scidownl/scidownl.py
+++ scidownl/scidownl.py
@@ -4,6 +4,7 @@ import argparse, os
 from .scihub import *
 from .update_link import *
 
+
 def main():
     """Command line tool to download pdfs via DOI from Scihub.
     """
@@ -13,23 +14,31 @@ def main():
     parser.add_argument('-o', '--output', help="directory to download the pdf")
     parser.add_argument('-u', '--update', action='store_true', help="update available Scihub links")
     parser.add_argument('-l', '--list', action='store_true', help="list current saved sichub urls.")
+    parser.add_argument('-c', '--choose', help="choose scihub url by index.")
     args = parser.parse_args()
 
     if args.DOI:
+        SCIHUB_URL_INDEX = int(open(get_resource_path('cur_scihub_index.txt'), 'r').read())
         if not args.output:
             sci = SciHub(args.DOI)
         else:
             sci = SciHub(args.DOI, args.output)
-        sci.download()
+        sci.download(choose_scihub_url_index=SCIHUB_URL_INDEX)
     elif args.update:
         update_link()
     elif args.list:
         link_file_path = get_resource_path('link.txt')
+        cur_scihub_index = int(open(get_resource_path('cur_scihub_index.txt'), 'r').read())
         if not os.path.isfile(link_file_path):
             open(link_file_path, 'w')
         with open(link_file_path, 'r') as link_file:
             for i, link in enumerate(link_file.readlines()):
-                print('[{0}] {1}'.format(i, link[:-1]))
+                if i == cur_scihub_index:
+                    print('* [{0}] {1}'.format(i, link[:-1]))
+                else:
+                    print('  [{0}] {1}'.format(i, link[:-1]))
+    elif args.choose:
+        open(get_resource_path('cur_scihub_index.txt'), 'w').write(args.choose)
     else:
         print("Command line tool to download pdfs via DOI from Scihub.")
 
